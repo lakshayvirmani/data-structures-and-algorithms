5e78512d2ca50d880e919761813eb5a6223c0bb4
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-05-15 08:23:33 +0530

    704. Binary Search

---
+++ binary_search/binary_search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left, right, mid;
+        
+        left = 0;
+        right = nums.size() - 1;
+        while(left <= right)
+        {
+            mid = left + (right - left)/2;
+            
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
