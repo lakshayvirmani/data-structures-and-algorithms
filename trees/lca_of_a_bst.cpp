6bf6c3557f331eb80906fad0609fda2f74b94d9a
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-05-25 12:38:55 +0530

    Added iterative solution for 235.

--- trees/lca_of_a_bst.cpp
+++ trees/lca_of_a_bst.cpp
@@ -29,4 +29,24 @@ public:
         
         return result;
     }
+
+    TreeNode* lowestCommonAncestorIterative(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root != NULL)
+        {
+            if(p->val < root->val && q->val < root->val)
+            {
+                root = root->left;
+            }
+            else if(p->val > root->val && q->val > root->val)
+            {
+                root = root->right;
+            }
+            else
+            {
+                return root;
+            }
+        }
+        
+        return root;
+    }
 };
