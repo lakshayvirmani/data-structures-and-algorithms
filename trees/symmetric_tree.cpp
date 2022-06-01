50fc54ef140b02ba703a8522b853e8d9db781e10
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-05-31 20:08:32 -0400

    101. Symmetric Tree

---
+++ trees/symmetric_tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkSymmetry(TreeNode* root1, TreeNode* root2)
+    {
+        if(root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        else if(root1 == NULL || root2 == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            return root1->val == root2->val && checkSymmetry(root1->left, root2->right) && checkSymmetry(root1->right, root2->left);
+        }
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return checkSymmetry(root, root);
+    }
+};
