6c485eca116346329ac76ccda9e92eec1707a72b
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-07-06 09:06:55 -0400

    199. Binary Tree Right Side View - Recursive

---
+++ trees/bt_right_side_view_rec.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        populateRightSideView(root, 1, result);
+        return result;
+    }
+private:
+    void populateRightSideView(TreeNode* root, int level, vector<int>& result)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        
+        if(result.size() < level)
+        {
+            result.push_back(root->val);
+        }
+        
+        populateRightSideView(root->right, level + 1, result);
+        populateRightSideView(root->left, level + 1, result);
+    }
+};
