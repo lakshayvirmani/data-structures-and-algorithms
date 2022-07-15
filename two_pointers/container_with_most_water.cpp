0b046a401f58e731eac1aaa29de44b036609708e
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-07-14 21:43:32 -0400

    11. Container With Most Water

---
+++ two_pointers/container_with_most_water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i = 0, j = n - 1;
+        int ans = 0;
+        
+        while(i < j)
+        {
+            ans = max(ans, min(height[i], height[j]) * (j - i));
+            
+            if(height[i] < height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        
+        return ans;
+    }
+};
