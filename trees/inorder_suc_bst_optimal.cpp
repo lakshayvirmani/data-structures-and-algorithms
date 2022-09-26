77b94a93b2c8ca9e017da61a2bf425830faade10
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2022-09-26 16:20:01 -0400

    285. Inorder Successor in BST - Direct search for the suc

---
+++ trees/inorder_suc_bst_optimal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        TreeNode* suc = NULL;
+        
+        while(root != NULL)
+        {
+            if(root->val > p->val)
+            {
+                suc = root;
+                root = root->left;
+            }
+            else
+            {
+                root = root->right;
+            }
+        }
+        
+        return suc;
+    }
+};
